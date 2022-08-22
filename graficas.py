e602b4281a49089009832383f3382f1c677443c9
Victor Arbiol <victor.arbiol@gmail.com>
2022-08-22 10:34:43 +0200

    Se sube mapa y se alade item, tambien busqueda de precio por slider

---
+++ graficas.py
@@ -0,0 +1,10 @@
+# importar librerias
+
+
+def yuan(df, token):
+    # adade tu codigo aqui
+    
+    primer_precio = df['price'][1]
+    print(f" primer precio en el DataFrame: {primer_precio}")
+
+
