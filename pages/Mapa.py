1d6aa5fd4b9d97fc090bbff985cff9008f9942f0
Victor Arbiol <victor.arbiol@gmail.com>
2022-09-01 11:56:28 +0200

    Mapa  update

--- pages/Mapa.py
+++ pages/Mapa.py
@@ -1,13 +1,16 @@
+from signal import pause
 import streamlit as st
 import folium
 import pandas as pd
 from PIL import Image
 from streamlit_folium import st_folium
-
+import time 
+#from streamlit_autorefresh import st_autorefresh
+#st_autorefresh(60000)
 
 # Comienza el programa
 def main():
-
+#@st.cache
     # Configuracion de css
     st.markdown('<style>' + open('./styles.css').read() +
                 '</style>', unsafe_allow_html=True)
@@ -19,7 +22,6 @@ Body = st.container()
 Footer = st.container()
 
 
-
 # Cabecera
 with Head:
     st.title('Buscador por precios')
@@ -28,10 +30,12 @@ with Head:
 
 # cuerpo de la pagina
 with Body:
-    #lectura de datos
+    # lectura de datos
     path_to_data = ('london_airbnb.csv')
 
 # Carga de datos y filtrado
+
+
 @st.cache(persist=True)
 def load_data():
     df = pd.read_csv(path_to_data)
@@ -50,33 +54,41 @@ leer_data = load_data()
 
 
 # Localización del mapa
-mapa = folium.Map(location=[51.509865, -0.118092], zoom_start=12)
-i = 0
+# pruebas mapa
 
-# Busca por precio de 10 hasta  500
-precios_londres = st.slider("Precio locales:", 10, 500)
+m = folium.Map(location=[51.509865, -0.118092], zoom_start=12)
+i=0
+#cuantos buscar
+# precio
+precios_londres = st.slider("Precio locales:", 10, 10000)
 
-# Ciclo de pintura jajajaja
-for i in range(len(leer_data['price'])):
+#print(len(leer_data['price']))
 
-    if precios_londres >= leer_data['price'][i]:
-        latitud = leer_data['latitude'][i]
-        longitud = leer_data['longitude'][i]
-        precio = leer_data['price'][i]
-        alquiler = leer_data['room_type'][i]
-        noches = leer_data['minimum_nights'][i]
-        visitas = leer_data['number_of_reviews'][i]
-        items = folium.Marker(
-            [latitud, longitud],
-            popup=f' valoración: {visitas}, noches {noches}, alquiler{alquiler}',
+
+def mapa():
+    for i in range(len(leer_data['price'])-1):
+    #for i in range(30000):
+        
+        if precios_londres > leer_data['price'][i]:
+            latitud = leer_data['latitude'][i]
+            longitud = leer_data['longitude'][i]
+            precio = leer_data['price'][i]
+            noches = leer_data['minimum_nights'][i]
+            visitas = leer_data['number_of_reviews'][i]
+            items = folium.Marker(
+            [latitud,longitud],
+            popup=f' valoración: {visitas}, noches {noches}',
             tooltip=f'{precio} libras'
-        ).add_to(mapa)
+        ).add_to(m)
+        print(f'iterando: {i}')
 
-# Renderizacion del mapa.
-st_data = st_folium(mapa, width=625)
+        # call to render Folium map in Streamlit
+    st_data = st_folium(m, width=725)
+    time.sleep(10) #ha dormir un rato
 
+mapa()
 
 # pie de pagina
 with Footer:
-    
+
     st.text('Mapa de resultados.')
